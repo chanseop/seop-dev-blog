1c01dc74af10fc9cca8b9465f924a5cb4f05f11d
chanseop <dlackstjq23@gmail.com>
2024-09-23 22:51:44 +0900

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Seop dev blog`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
