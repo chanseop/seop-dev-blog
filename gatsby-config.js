59d9c85dee40f80f93ff6615ca63f28ce7965633
chanseop <dlackstjq23@gmail.com>
2024-09-26 22:12:28 +0900

    deploy config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,19 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+
+const adapter = require("gatsby-adapter-netlify").default
 module.exports = {
   siteMetadata: {
     title: `Seop dev blog`,
     siteUrl: `https://www.yourdomain.tld`,
   },
+  adapter: adapter({
+    excludeDatastoreFromEngineFunction: false,
+    imageCDN: false,
+  }),
   plugins: [],
 }
+
+
+
